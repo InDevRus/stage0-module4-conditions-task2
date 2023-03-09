9bf9b06f1f5f0ad37b93d1f39f9a13b12b408a33
InDev <InDevRus@hotmail.com>
2023-03-09 16:56:14 +0500

    Solved

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        var LAST_EAGLE = 500;
+        var coinFlip = from1UpTo1000 < LAST_EAGLE ? "Eagle" : "Tail";
+        System.out.println(coinFlip);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        var bigger = first > second ? first : second;
+        System.out.println(bigger);
     }
 }
